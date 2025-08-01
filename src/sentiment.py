cb2cf122cf61485b848754f8e45013a8cdaea3e1
lmalmud <lmalmud2@jh.edu>
2025-08-01 17:40:44 -0400

    computed bull ratio

--- src/sentiment.py
+++ src/sentiment.py
@@ -6,19 +6,17 @@ Source:
 https://medium.com/@ravirajshinde2000/financial-news-sentiment-analysis-using-finbert-25afcc95e65f
 '''
 
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
 import pandas as pd
 import numpy as np
 import torch
 
 # Load pre-trained FinBERT model and tokenizer
-finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
-tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
 
 reddit_parsed = pd.read_csv('../data/reddit_wsb_parsed.csv')
-
-# labels = {0:'neutral', 1:'positive',2:'negative'}
-reorderings = {0: 1, 1: 0, 2: 2} # so rankings are positive -> negative
 
 def get_sentiment(txt):
     '''
@@ -30,30 +28,37 @@ def get_sentiment(txt):
 
     '''
 
-    # Inputs is a dictionary containing the tokenized and encoded version of text
-    inputs = tokenizer(txt,
-                       return_tensors='pt',
-                       padding=True,
-                       truncation=True, # Handles long texts
-                       max_length=512)
+    # encoded_input is a dictionary containing the tokenized and encoded version of text
+    # Need to include truncation and max_length, otherwise model does not work
+    encoded_input = tokenizer(txt,
+                            return_tensors='pt',
+                            truncation=True, 
+                            max_length=512)
     
-    with torch.no_grad():  # Disable gradient computation for inference
-        outputs = finbert(**inputs)
-        logits = outputs.logits
-        # Logits are raw, unnormalized output scores before any final processing
-        # Will be something like [[-.5, 2.1, -1.3]], representing the scores for index 0 (neutral)
-        # index 1 (positive), and index 2 (negative) -- we want the highest
-        # The logits are not probabilities
-        predicted_class = torch.argmax(logits, dim=1).item() # Gets the predicted class as integer
-    
-    return predicted_class
+    # Get the model output
+    output = model(**encoded_input)
 
-def convert_sentiment(x):
-    return reorderings[x]
+    # Apply softmax to get probabilities
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+
+    # Calculate the compound score
+    # The order of labels is [positive, negative, neutral]
+    compound_score = scores[0] - scores[1]  # Positive - Negative
+    return compound_score
+
 
 reddit_parsed['sentiment'] = reddit_parsed['raw_text'].apply(get_sentiment)
 
-# Convert the scores to appropriate numerical values
-reddit_parsed['sentiment'].apply(convert_sentiment)
-
 reddit_parsed.to_csv('../data/reddit_wsb_with_sentiment.csv', index=False)
+
+# TESTS
+'''
+text = "The company's stock price has surged recently due to positive earnings reports."
+compound_score = get_sentiment(text)
+print(f"FinBERT Compound Score: {compound_score}")
+
+text2 = "The company's financial outlook is bleak with declining revenues."
+compound_score2 = get_sentiment(text2)
+print(f"FinBERT Compound Score: {compound_score2}")
+'''
